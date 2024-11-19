c3cd006296d8729521c57ff41a835ced0679cc74
Harshit Yadav <2105891@kiit.ac.in>
2024-11-19 15:35:17 +0530

    feat: LeftSidebar created

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import LeftSidebar from "./components/LeftSidebar";
 const MainLayout = () => {
 
     // checking for mobile device
@@ -19,7 +20,14 @@ const MainLayout = () => {
             <ResizablePanelGroup direction="horizontal" className='flex-1 flex h-full overflow-hidden p-2'>
                 {/* Audio Player */}
 
+                {/* left sidebar */}
+                <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+                    <LeftSidebar />
+                </ResizablePanel>
+
                 <ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+
+
                 {/* Main content */}
                 <ResizablePanel defaultSize={isMobile ? 80 : 60}>
                     <Outlet />
@@ -32,6 +40,7 @@ const MainLayout = () => {
                         {/* right sidebar */}
                         <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
                             {/* Friends Activity */}
+                            Friends activity
                         </ResizablePanel>
                     </>
                 )}
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -9,3 +9,34 @@ export interface Song {
   createdAt: string;
   updatedAt: string;
 }
+
+/*
+[
+{
+"_id" : "1",
+"title" : "Album title",
+"artist" : "Album Artist",
+"imageUrl": "https://i.scdn.co/image/ab67616d0000b273f7b4c1b7a5f8f8d9f8f5b7b3",
+"releaseYear" : 2004,
+"songs": [
+{
+	"_id": "61f2e4d9b3d6a1c6c0b5d6c9",
+	"title": "The Way You Look Tonight",
+	"artist": "Frank Sinatra",
+	"albumId": "61f2e4d9b3d6a1c6c0b5d6c8",
+	"imageUrl": "https://i.scdn.co/image/ab67616d0000b273f7b4c1b7a5f8f8d9f8f5b7b3",
+	"audioUrl": "https://res.cloudinary.com/d
+}
+]
+}
+]
+*/
+
+export interface Album {
+  _id: string;
+  title: string;
+  artist: string;
+  imageUrl: string;
+  releaseYear: number;
+  songs: Song[];
+}
