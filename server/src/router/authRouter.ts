a7accbc27bf3ecb614ee2622c3ab1a1c01212de0
Harshit Yadav <2105891@kiit.ac.in>
2024-11-26 13:13:11 +0530

    feat: Auth controller updated

--- server/src/router/authRouter.ts
+++ server/src/router/authRouter.ts
@@ -1,32 +1,31 @@
 import { Router } from 'express'
-import apiController from '../controller/apiController'
-import { register } from '../controller/userController'
+import authController from '../controller/authController'
 import authentication from '../middleware/authentication'
 import rateLimit from '../middleware/rateLimit'
 
 const router = Router()
 
-router.route('/self').get(apiController.self)
-router.route('/health').get(apiController.health)
+router.route('/self').get(authController.self)
+router.route('/health').get(authController.health)
 
 // Authentication routes
-router.route('/register').post(rateLimit, register)
+router.route('/register').post(rateLimit, authController.register)
 
-router.route('/confirmation/:token').put(rateLimit, apiController.confirmation)
+router.route('/confirmation/:token').put(rateLimit, authController.confirmation)
 
-router.route('/login').post(rateLimit, apiController.login)
+router.route('/login').post(rateLimit, authController.login)
 
-router.route('/self-identification').get(authentication, apiController.selfIdentification)
+router.route('/self-identification').get(authentication, authController.selfIdentification)
 
-router.route('/logout').put(authentication, apiController.logout)
+router.route('/logout').put(authentication, authController.logout)
 
-router.route('/refresh-token').post(rateLimit, apiController.refreshToken)
+router.route('/refresh-token').post(rateLimit, authController.refreshToken)
 
-router.route('/forgot-password').put(rateLimit, apiController.forgotPassword)
+router.route('/forgot-password').put(rateLimit, authController.forgotPassword)
 
-router.route('/reset-password/:token').put(rateLimit, apiController.resetPassword)
+router.route('/reset-password/:token').put(rateLimit, authController.resetPassword)
 
-router.route('/change-password').put(authentication, apiController.changePassword)
+router.route('/change-password').put(authentication, authController.changePassword)
 
 export default router
 
