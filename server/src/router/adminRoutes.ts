4efe5f8a59cf694f968eb98baa6533d61c36ef96
Harshit Yadav <2105891@kiit.ac.in>
2024-12-11 00:20:56 +0530

    feat: Migrate from TS to JS

--- server/src/router/adminRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { createSong } from '../controller/adminController'
-
-const router = Router()
-
-router.post('/songs', createSong)
-
-export default router
-
